4e2dd2528d13eb64d2bc34af35b70434e76e55fd
Christopher Lin <c.l@berkeley.edu>
2016-04-01 18:36:34 -0700

    added comparison expressions, hinge expression and absolute value expression. updated documentation in expr

--- src/expr.py
+++ src/expr.py
@@ -1,4 +1,5 @@
-import numpy as np, numdifftools as nd
+import numpy as np
+import numdifftools as nd
 
 """
 Utility classes to represent expresions. Each expression defines an eval, grad,
@@ -27,8 +28,9 @@ class Expr(object):
 
     def convexify(self, x, degree=1):
         """
-        Returns an convex approximation of the expression at x where degree 1 is
-        an affine approximation and degree 2 is a quadratic approximation
+        Returns an Expression object that represents the convex approximation of
+        self at x where degree 1 is an affine approximation and degree 2 is a
+        quadratic approximation
         """
 
         if degree == 1:
@@ -40,6 +42,9 @@ class Expr(object):
 
 
 class AffExpr(Expr):
+    """
+    Affine Expression
+    """
 
     def __init__(self, A, b):
         """
@@ -59,7 +64,10 @@ class AffExpr(Expr):
         return 0.0
 
 
-class QuadExpr(AffExpr):
+class QuadExpr(Expr):
+    """
+    Quadratic Expression
+    """
 
     def __init__(self, Q, A, b):
         """
@@ -79,23 +87,128 @@ class QuadExpr(AffExpr):
     def hess(self, x):
         return self.Q.copy()
 
+class AbsExpr(Expr):
+    """
+    Absolute value expression
+    """
+
+    def __init__(self, expr):
+        self.expr = expr
+
+    def eval(self, x):
+        return np.absolute(self.expr.eval(x))
+
+    def grad(self, x):
+        """
+        Since the absolute value expression is not smooth, a subgradient is
+        returned instead of the gradient.
+        """
+        raise NotImplementedError
+
+class HingeExpr(Expr):
+    """
+    Hinge expression
+    """
+
+    def __init__(self, expr):
+        self.expr = expr
+
+    def eval(self, x):
+        v = self.expr.eval(x)
+        zeros = np.zeros(v.shape)
+        return np.maximum(v, zeros)
+
+    def grad(self, x):
+        """
+        Since the hinge expression is not smooth, a subgradient is returned
+        instead of the gradient.
+        """
+        raise NotImplementedError
+
+class CompExpr(Expr):
+    """
+    Comparison Expression
+    """
+
+    def __init__(self, expr, val):
+        """
+        expr: Expr object, the expression that is being compared to val
+        val: numpy array, the value that the expression is being compared to
+        """
+        self.expr = expr
+        self.val = val
+
+    def eval(self, x):
+        """
+        Returns 1 if the comparison holds true within some tolerace and 0
+        otherwise.
+        """
+        raise NotImplementedError
+
+    def grad(self, x):
+        raise Exception("The gradient is not well defined for comparison
+            expressions")
+
+    def convexify(self, x, degree=1):
+        raise NotImplementedError
+
+class EqExpr(Expr):
+    """
+    Equality Expression
+    """
+
+    def eval(self, x, tol=DEFAULT_TOL):
+        """
+        Tests whether the expression at x is equal to self.val with tolerance
+        tol.
+        """
+        raise NotImplementedError
+
+    def convexify(self, x):
+        """
+        Returns an AbsExpr that is the l1 penalty expression, a measure of
+        constraint violation.
+
+        The constraint h(x) = 0 becomes |h(x)|
+        """
+        aff_expr = self.expr.convexify(x, degree=1)
+        aff_expr.b = aff_expr.b - self.val
+        return AbsExpr(aff_expr)
+
+class LEqExpr(Expr):
+    """
+    Less than or equal to expression
+    """
+
+    def eval(self, x, tol=DEFAULT_TOL):
+        """
+        Tests whether the expression at x is less than or equal to self.val with
+        tolerance tol.
+        """
+        raise NotImplementedError
+
+    def convexify(self, x):
+        """
+        Returns a HingeExpr that is the hinge penalty expression, a measure of
+        constraint violation.
+
+        The constraint g(x) <= 0 becomes |g(x)|+ where |g(x)|+ = max(g(x), 0)
+        """
+        aff_expr = self.expr.convexify(x, degree=1)
+        aff_expr.b = aff_expr.b - self.val
+        return HingeExpr(aff_expr)
 
 class BoundExpr(object):
     """
     Bound expression
 
-    Bound expression is composed of an Expr and a Variable
+    Bound expression is composed of an Expr and a Variable. Please note that the
+    variable ordering matters
     """
 
     def __init__(self, expr, var):
         self.expr = expr
         self.var = var
-
-    def grb_expr(self):
-        """
-        Returns the Gurobi expression
-        """
-        return self.expr.eval(self.var.get_grb_vars())
 
     def eval(self):
         """
@@ -107,8 +220,8 @@ class BoundExpr(object):
         """
         Returns a convexified BoundExpr at the variable's current value.
         """
-        convexified_expr = self.expr.convexify(self.var.get_value(), degree)
-        return BoundExpr(convexified_expr, self.var)
+        cvx_expr = self.expr.convexify(self.var.get_value(), degree)
+        return BoundExpr(cvx_expr, self.var)
 
 
 class TFExpr(Expr):
