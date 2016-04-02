76b008289f1ebe14fb879dee88944263b741a41b
Christopher Lin <c.l@berkeley.edu>
2016-04-01 18:34:19 -0700

    added convergence test skeleton code to solver

--- src/solver.py
+++ src/solver.py
@@ -18,11 +18,30 @@ class Solver(object):
         Uses Penalty Sequential Quadratic Programming to solve the problem
         instance.
         """
-
-        pass
+        raise NotImplementedError
 
     def _min_merit_fn(self, prob, penalty_coeff, trust_region_size):
         """
         Minimize merit function for penalty sqp
         """
-        pass
+        raise NotImplementedError
+
+    def _is_converged(self, prob, trust_region_size):
+        """
+        Returns true if prob has converged
+        """
+        raise NotImplementedError
+
+    def _x_converged(self, prob, trust_region_size):
+        """
+        Returns true if the variable values has converged (trust_region size is
+        smaller than the minimum trust box size)
+        """
+        raise NotImplementedError
+
+    def _y_converged(self, prob, trust_region_size):
+        """
+        Returns true if the approx_merit has converged (approx_merit <
+        min_approx_merit_improve)
+        """
+        raise NotImplementedError
