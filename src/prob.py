f489608e2c7f43f9dd8abfceb6ee5fb8ff934f96
c-l <c.l@berkeley.edu>
2016-05-27 16:46:03 -0700

    initial implementation of prob and prob tests

--- src/prob.py
+++ src/prob.py
@@ -1,58 +1,63 @@
 import gurobipy as grb
+GRB = grb.GRB
 from expr import *
 from ipdb import set_trace as st
+
 
 class Prob(object):
     """
     Sequential convex programming problem. A solution is found using the l1
-    penalty method. In the this class, cvx (convex) refers to quadratic
-    objectives and linear constraints.
+    penalty method.
     """
 
     def __init__(self):
         """
         _model: Gurobi model associated with this problem
         _vars: variables in this problem
-        _obj_exprs: list of expressions in the objective, the sum of the
-            expressions represent the objective
-        _cnt_exprs: list of constraint expressions
 
-        The l1 penalty objective is the sum of the quadratic approximation
-        of the objective (_quad_obj_exprs) and the penalty terms created
-        by approximating the constraints (_penalty_exprs)
-        _quad_obj_exprs: list of expressions in the quadratic approximation of
-            the objective, the sum of these expressions represents the current
-            quadratic approximation of the objective
-        _penalty_exprs: list of penalty term expressions
+        _quad_obj_exprs: list of quadratic expressions in the objective
+        _nonquad_obj_exprs: list of non-quadratic expressions in the objective
+        _approx_obj_exprs: list of approximation of the non-quadratic
+            expressions in the objective
+
+        _nonlin_cnt_exprs: list of non-linear constraint expressions
+        _penalty_exprs: list of penalty term expressions (approximations of the
+            non-linear constraint expressions in _nonlin_cnt_exprs)
+
+        _grb_penalty_cnts: list of Gurobi constraints that are generated when
+            adding the hinge and absolute value terms from the penalty terms.
+        _pgm: Positive Gurobi variable manager provides a lazy way of generating
+            positive Gurobi variables so that there are less model updates.
         """
         self._model = grb.Model()
         self._vars = set()
 
-        self._cvx_obj_exprs = []
-        self._noncvx_obj_exprs = []
+        self._quad_obj_exprs = []
+        self._nonquad_obj_exprs = []
         self._approx_obj_exprs = []
 
-        # convex constraints are added directly to the model so there's no
-        # need for a _cvx_cnt_exprs variable
-        self._noncvx_cnt_exprs = []
+        # linear constraints are added directly to the model so there's no
+        # need for a _lin_cnt_exprs variable
+        self._nonlin_cnt_exprs = []
 
         self._penalty_exprs = []
         self._grb_penalty_cnts = [] # hinge and abs value constraints
+        self._pgm = PosGRBVarManager(self._model)
 
     def add_obj_expr(self, bound_expr):
         """
         Adds a bound expression (bound_expr) to the objective. If the objective
-        is quadratic, is it added directly to the model. Otherwise, it is added
-        by appending bound_expr to self._obj_exprs.
+        is quadratic, is it added to _quad_obj_exprs. Otherwise, it is added
+        to self._nonquad_obj_exprs.
 
         bound_expr's var is added to self._vars so that a trust region can be
         added to var.
         """
         expr = bound_expr.expr
         if isinstance(expr, AffExpr) or isinstance(expr, QuadExpr):
-            self._cvx_obj_exprs.append(bound_expr)
+            self._quad_obj_exprs.append(bound_expr)
         else:
-            self._noncvx_obj_exprs.append(bound_expr)
+            self._nonquad_obj_exprs.append(bound_expr)
         self._vars.add(bound_expr.var)
 
     def add_cnt_expr(self, bound_expr):
@@ -60,7 +65,7 @@ class Prob(object):
         Adds a bound expression (bound_expr) to the problem's constraints.
         If the constraint is linear, it is added directly to the model.
         Otherwise, the constraint is added by appending bound_expr to
-        self._cnt_exprs.
+        self._nonlin_cnt_exprs.
 
         bound_expr's var is added to self._vars so that a trust region can be
         added to var.
@@ -71,20 +76,32 @@ class Prob(object):
         assert isinstance(comp_expr, CompExpr)
         if isinstance(expr, AffExpr):
             # adding constraint directly into model
-            grb_expr = self._aff_expr_to_grb_expr(expr, var)
+            grb_expr, grb_cnt = self._aff_expr_to_grb_expr(expr, var)
             if isinstance(comp_expr, EqExpr):
                 self._add_np_array_grb_cnt(grb_expr, GRB.EQUAL, comp_expr.val)
             elif isinstance(comp_expr, LEqExpr):
                 self._add_np_array_grb_cnt(grb_expr, GRB.LESS_EQUAL, comp_expr.val)
         else:
-            self._noncvx_cnt_exprs.append(bound_expr)
+            self._nonlin_cnt_exprs.append(bound_expr)
         self._vars.add(var)
 
     def _add_np_array_grb_cnt(self, grb_exprs, sense, val):
+        """
+        Adds a numpy array of Gurobi constraints to the model and returns
+        the constraints.
+        """
+        cnts = []
         for index, grb_expr in np.ndenumerate(grb_exprs):
-            self._model.addConstr(grb_expr, sense, val[index])
+            cnts.append(self._model.addConstr(grb_expr, sense, val[index]))
+        return cnts
 
     def _expr_to_grb_expr(self, bound_expr):
+        """
+        Translates AffExpr, QuadExpr, HingeExpr and AbsExpr to Gurobi
+        expressions and returns the corresponding Gurobi expressions and
+        constraints. If there are Gurobi constraints, an empty list is returned.
+        Otherwise, this method raises an exception.
+        """
         expr = bound_expr.expr
         var = bound_expr.var
 
@@ -92,16 +109,43 @@ class Prob(object):
             return self._aff_expr_to_grb_expr(expr, var)
         elif isinstance(expr, QuadExpr):
             return self._quad_expr_to_grb_expr(expr, var)
-
-    def _quad_expr_to_grb_expr(self, quad_expr, var):
-        x = var.get_grb_vars()
-        return x.T.dot(quad_expr.Q.dot(x)) + quad_expr.A.dot(x) + quad_expr.b
+        elif isinstance(expr, HingeExpr):
+            return self._hinge_expr_to_grb_expr(expr, var)
+        elif isinstance(expr, AbsExpr):
+            return self._abs_expr_to_grb_expr(expr, var)
+        elif isinstance(expr, CompExpr):
+            raise Exception("Comparison Expressions cannot be converted to \
+                a Gurobi expression. Use add_cnt_expr instead")
+        else:
+            raise Exception("This type of Expression cannot be converted to\
+                a Gurobi expression.")
 
     def _aff_expr_to_grb_expr(self, aff_expr, var):
         grb_var = var.get_grb_vars()
-        return aff_expr.A.dot(grb_var) + aff_expr.b
+        return aff_expr.A.dot(grb_var) + aff_expr.b, []
 
-    def optimize(self):
+    def _quad_expr_to_grb_expr(self, quad_expr, var):
+        x = var.get_grb_vars()
+        return x.T.dot(quad_expr.Q.dot(x)) + quad_expr.A.dot(x) + quad_expr.b, []
+
+    def _hinge_expr_to_grb_expr(self, hinge_expr, var):
+        aff_expr = hinge_expr.expr
+        assert isinstance(aff_expr, AffExpr)
+        grb_expr, _ = self._aff_expr_to_grb_expr(aff_expr, var)
+        grb_hinge = self._pgm.get_array(grb_expr.shape)
+        cnts = self._add_np_array_grb_cnt(grb_expr, GRB.LESS_EQUAL, grb_hinge)
+        return grb_hinge, cnts
+
+    def _abs_expr_to_grb_expr(self, abs_expr, var):
+        aff_expr = abs_expr.expr
+        assert isinstance(aff_expr, AffExpr)
+        grb_expr, _ = self._aff_expr_to_grb_expr(aff_expr, var)
+        pos = self._pgm.get_array(grb_expr.shape)
+        neg = self._pgm.get_array(grb_expr.shape)
+        cnts = self._add_np_array_grb_cnt(grb_expr, GRB.EQUAL, pos-neg)
+        return pos+neg, cnts
+
+    def optimize(self, penalty_coeff=0.0):
         """
         Calls the Gurobi optimizer on the current QP approximation
 
@@ -118,69 +162,29 @@ class Prob(object):
         The Gurobi constraints are the linear constraints which have already
         been added to the model when constraints were added to this problem.
         """
+        self._del_old_grb_cnts()
         self._model.update()
-        obj_exprs = [self._expr_to_grb_expr(bound_expr) for bound_expr in self._cvx_obj_exprs]
-        for obj_expr in obj_exprs:
-            assert obj_expr.shape == (1,1)
-        obj_exprs = [obj_expr[0,0] for obj_expr in obj_exprs]
-        obj = grb.quicksum(obj_exprs)
+
+        grb_exprs = []
+        for bound_expr in self._quad_obj_exprs + self._approx_obj_exprs:
+            grb_expr, grb_cnts = self._expr_to_grb_expr(bound_expr)
+            self._grb_penalty_cnts.extend(grb_cnts)
+            grb_exprs.extend(grb_expr.flatten().tolist())
+
+        for bound_expr in self._penalty_exprs:
+            grb_expr, grb_cnts = self._expr_to_grb_expr(bound_expr)
+            grb_expr *= penalty_coeff
+            self._grb_penalty_cnts.extend(grb_cnts)
+            grb_exprs.extend(grb_expr.flatten().tolist())
+
+        obj = grb.quicksum(grb_exprs)
         self._model.setObjective(obj)
         self._model.optimize()
         self._update_vars()
 
-        raise NotImplementedError
-
-    def _add_expr_to_grb_obj(self, bound_expr):
-        """
-        Adds a BoundExpr to self's Gurobi model's objective.
-        """
-        expr = bound_expr.expr
-        var = bound_expr.var
-
-        if isinstance(expr, AffExpr):
-            self._add_aff_expr_to_grb_model(expr, var)
-        elif isinstance(expr, QuadExpr):
-            self._add_quad_expr_to_grb_model(expr, var)
-        elif isinstance(expr, HingeExpr):
-            self._add_hinge_expr_to_grb_model(expr, var)
-        elif isinstance(expr, AbsExpr):
-            self._add_abs_expr_to_grb_model(expr, var)
-        elif isinstance(expr, CompExpr):
-            raise Exception("Comparison Expressions can't be added to the \
-                Gurobi model")
-
-    def _add_aff_expr_to_grb_model(self, aff_expr, var):
-        """
-        Adds an affine expression with var into self's Gurobi model's
-        objective.
-        """
-        raise NotImplementedError
-
-    def _add_quad_expr_to_grb_model(self, quad_expr, var):
-        """
-        Adds a quadratic expression with var into self's Gurobi model's
-        objective.
-        """
-        raise NotImplementedError
-
-    def _add_hinge_expr_to_grb_model(self, hinge_expr, var):
-        """
-        Adds a hinge expression with var into self's Gurobi model's
-        objective by creating additional Gurobi variables and constraints
-        to represent the hinge. The created Gurobi variables and
-        constraints are saved so that they can be removed later.
-        """
-        raise NotImplementedError
-
-    def _add_abs_expr_to_grb_model(self, abs_expr, var):
-        """
-        Adds an absolute value expression with var into self's Gurobi
-        model's objective by creating additional Gurobi variables and
-        constraints to represent the absolute value. The created Gurobi
-        variables and constraints are saved so that they can be removed
-        later.
-        """
-        raise NotImplementedError
+    def _del_old_grb_cnts(self):
+        for cnt in self._grb_penalty_cnts:
+            self._model.remove(cnt)
 
     def add_trust_region(self, trust_region_size):
         """
@@ -189,38 +193,63 @@ class Prob(object):
         for var in self._vars:
             var.add_trust_region(trust_region_size)
 
-    def convexify(self, penalty_coeff):
+    def convexify(self):
         """
         Convexifies the optimization problem by computing a QP approximation
-        A quadratic approximation of the objective (self._obj_exprs) is saved
-        in self._quad_obj_exprs.
-        The penalty approximation of the constraints (self._cnt_exprs) is saved
-        in self._penalty_exprs
+        A quadratic approximation of the non-quadratic objective terms
+        (self._nonquad_obj_exprs) is saved in self._approx_obj_exprs.
+        The penalty approximation of the non-linear constraints
+        (self._nonlin_cnt_exprs) is saved in self._penalty_exprs
         """
-        raise NotImplementedError
-
+        self._approx_obj_exprs = [bexpr.convexify(degree = 2) \
+            for bexpr in self._nonquad_obj_exprs]
+        self._penalty_exprs = [bexpr.convexify(degree = 1) \
+            for bexpr in self._nonlin_cnt_exprs]
 
     def get_value(self, penalty_coeff):
         """
         Returns the current value of the penalty objective.
         The penalty objective is computed by summing up all the values of the
-        objective expressions self._obj_exprs and the penalty values of
-        constraint expressions self._cnt_exprs.
+        quadratic objective expressions (self._quad_obj_exprs), the
+        non-quadratic objective expressions and the penalty coeff multiplied
+        by the constraint violations (computed using _nonlin_cnt_exprs)
         """
-        raise NotImplementedError
+        value = 0.0
+        for bound_expr in self._quad_obj_exprs + self._nonquad_obj_exprs:
+            value += np.sum(bound_expr.eval())
+        for bound_expr in self._nonlin_cnt_exprs:
+            cnt_vio = self._compute_cnt_violation(bound_expr)
+            value += penalty_coeff*np.sum(cnt_vio)
+        return value
+
+    def _compute_cnt_violation(self, bexpr):
+        comp_expr = bexpr.expr
+        var_val = bexpr.var.get_value()
+        if isinstance(comp_expr, EqExpr):
+            return np.absolute(comp_expr.expr.eval(var_val) - comp_expr.val)
+        elif isinstance(comp_expr, LEqExpr):
+            v = comp_expr.expr.eval(var_val) - comp_expr.val
+            zeros = np.zeros(v.shape)
+            return np.maximum(v, zeros)
 
     def get_approx_value(self, penalty_coeff):
         """
         Returns the current value of the penalty QP approximation by summing
-        up the expression values in the convexified objective self._penalty_obj
-        and expressions values of penalty terms for the constraints in
-        self._convexified_obj_exprs. Note that this approximate value is
-        computed with respect to when the last convexification was performed.
+        up the expression values for the quadratic objective terms
+        (_quad_obj_exprs), the quadratic approximation of the non-quadratic
+        terms (_approx_obj_exprs) and the penalty terms (_penalty_exprs).
+        Note that this approximate value is computed with respect to when the
+        last convexification was performed.
 
         TODO: check that get_approx_value returns the same value as Gurobi's
         model objVal.
         """
-        raise NotImplementedError
+        value = 0.0
+        for bound_expr in self._quad_obj_exprs + self._approx_obj_exprs:
+            value += np.sum(bound_expr.eval())
+        for bound_expr in self._penalty_exprs:
+            value += penalty_coeff*np.sum(bound_expr.eval())
+        return value
 
     def _update_vars(self):
         """
@@ -243,3 +272,52 @@ class Prob(object):
         """
         for var in self._vars:
             var.restore()
+
+class PosGRBVarManager(object):
+    """
+    Manages positive Gurobi variables. The purpose of the manager is to create
+    many Gurobi variables at once to decrease the number of Gurobi model update
+    because model updates take a long time.
+    """
+    INIT_NUM = 1000
+    INC_NUM = 1000
+
+    def __init__(self, model, init_num=INC_NUM, inc_num=INC_NUM):
+        self._index = 0
+        self._model = model
+        self._grb_vars = []
+        self._add_grb_vars(init_num)
+        self._inc_num = inc_num
+
+    def _add_grb_vars(self, num=None):
+        """
+        Creates a batch of positive Gurobi variables so that the model is
+        updated less often.
+        """
+        if num is None:
+            num = self._inc_num
+        new_grb_vars = [self._model.addVar(lb=0.0, ub=GRB.INFINITY) \
+                            for i in range(num)]
+        self._grb_vars.extend(new_grb_vars)
+        self._model.update()
+
+    def next(self):
+        """
+        Returns one positive Gurobi variable.
+        """
+        if self._index == len(self._grb_vars)-1:
+            self._add_grb_vars()
+        self._index += 1
+        return self._grb_vars[self._index-1]
+
+    def get_array(self, shape):
+        """
+        Returns a numpy array of unused positive Gurobi variables.
+        """
+        a = np.empty(shape, dtype=object)
+        for x in np.nditer(a, op_flags=['readwrite'], flags=['refs_ok']):
+            x[...] = self.next()
+        return a
+
+    def reset():
+        self._index = 0
--- src/test/test_prob.py
+++ src/test/test_prob.py
@@ -3,7 +3,7 @@ import gurobipy as grb
 GRB = grb.GRB
 
 import numpy as np
-from prob import Prob
+from prob import Prob, PosGRBVarManager
 from expr import *
 from variable import Variable
 
@@ -12,6 +12,8 @@ from ipdb import set_trace as st
 
 f = lambda x: np.array([[x]])
 
+def test_grb_var_pos(ut, grb_var):
+    ut.assertTrue(grb_var.lb == 0.0 and grb_var.ub == GRB.INFINITY)
 
 class TestProb(unittest.TestCase):
 
@@ -29,11 +31,11 @@ class TestProb(unittest.TestCase):
         prob.add_obj_expr(bexpr_quad)
         prob.add_obj_expr(bexpr_aff)
 
-        self.assertTrue(bexpr_aff in prob._cvx_obj_exprs)
-        self.assertTrue(bexpr_quad in prob._cvx_obj_exprs)
+        self.assertTrue(bexpr_aff in prob._quad_obj_exprs)
+        self.assertTrue(bexpr_quad in prob._quad_obj_exprs)
         self.assertTrue(var in prob._vars)
 
-    def test_add_obj_expr_noncvx(self):
+    def test_add_obj_expr_nonquad(self):
         expr = Expr(f)
         prob = Prob()
         model = prob._model
@@ -44,8 +46,8 @@ class TestProb(unittest.TestCase):
         bexpr = BoundExpr(expr, var)
         prob.add_obj_expr(bexpr)
 
-        self.assertTrue(bexpr not in prob._cvx_obj_exprs)
-        self.assertTrue(bexpr in prob._noncvx_obj_exprs)
+        self.assertTrue(bexpr not in prob._quad_obj_exprs)
+        self.assertTrue(bexpr in prob._nonquad_obj_exprs)
         self.assertTrue(var in prob._vars)
 
 
@@ -63,8 +65,8 @@ class TestProb(unittest.TestCase):
         prob.add_obj_expr(bexpr_quad)
         prob.add_obj_expr(bexpr_aff)
 
-        self.assertTrue(bexpr_aff in prob._cvx_obj_exprs)
-        self.assertTrue(bexpr_quad in prob._cvx_obj_exprs)
+        self.assertTrue(bexpr_aff in prob._quad_obj_exprs)
+        self.assertTrue(bexpr_quad in prob._quad_obj_exprs)
         self.assertTrue(var in prob._vars)
 
         prob.optimize()
@@ -106,6 +108,236 @@ class TestProb(unittest.TestCase):
 
         prob.optimize()
         self.assertTrue(np.allclose(var.get_value(), np.array([[-4]])))
+
+    def test_hinge_expr_to_grb_expr1(self):
+        """
+        min max(0, x+1) st. x == -4
+        """
+        aff = AffExpr(np.ones((1,1)), np.ones((1,1)))
+        hinge = HingeExpr(aff)
+        prob = Prob()
+        model = prob._model
+
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+        model.update()
+
+        hinge_grb_expr, hinge_grb_cnt = prob._hinge_expr_to_grb_expr(hinge, var)
+        model.update()
+        obj = hinge_grb_expr[0,0]
+        prob._model.setObjective(obj)
+
+        aff = AffExpr(np.ones((1,1)), np.zeros((1,1)))
+        comp = EqExpr(aff, np.array([[-4]]))
+        bound_expr = BoundExpr(comp, var)
+        prob.add_cnt_expr(bound_expr)
+
+        prob._model.optimize()
+        var.update()
+        self.assertTrue(np.allclose(var.get_value(), np.array([[-4]])))
+        self.assertTrue(np.allclose(obj.X, 0.0))
+
+    def test_hinge_expr_to_grb_expr2(self):
+        """
+        min max(0, x+1) st. x == 1
+        """
+        aff = AffExpr(np.ones((1,1)), np.ones((1,1)))
+        hinge = HingeExpr(aff)
+        prob = Prob()
+        model = prob._model
+
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+        model.update()
+
+        hinge_grb_expr, hinge_grb_cnt = prob._hinge_expr_to_grb_expr(hinge, var)
+        model.update()
+        obj = hinge_grb_expr[0,0]
+        prob._model.setObjective(obj)
+
+        aff = AffExpr(np.ones((1,1)), np.zeros((1,1)))
+        comp = EqExpr(aff, np.array([[1.0]]))
+        bound_expr = BoundExpr(comp, var)
+        prob.add_cnt_expr(bound_expr)
+
+        prob._model.optimize()
+        var.update()
+        self.assertTrue(np.allclose(var.get_value(), np.array([[1.0]])))
+        self.assertTrue(np.allclose(obj.X, 2.0))
+
+    def test_abs_expr_to_grb_expr(self):
+        """
+        min |x + 1| s.t. x <= -4
+        """
+        aff = AffExpr(np.ones((1,1)), np.ones((1,1)))
+        abs_expr = AbsExpr(aff)
+
+        aff = AffExpr(np.ones((1,1)), np.zeros((1,1)))
+        comp = LEqExpr(aff, np.array([[-4]]))
+
+        prob = Prob()
+        model = prob._model
+
+
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+        model.update()
+
+        abs_grb_expr, abs_grb_cnt = prob._abs_expr_to_grb_expr(abs_expr, var)
+        model.update()
+        prob._model.setObjective(abs_grb_expr[0,0])
+
+        bexpr = BoundExpr(comp, var)
+        prob.add_cnt_expr(bexpr)
+
+        prob._model.optimize()
+        var.update()
+        self.assertTrue(np.allclose(var.get_value(), np.array([[-4]])))
+        pos = abs_grb_expr[0,0].getVar(0).X
+        neg = abs_grb_expr[0,0].getVar(1).X
+        self.assertTrue(np.allclose(pos, 0.0))
+        self.assertTrue(np.allclose(neg, 3.0))
+
+    def test_convexify_eq(self):
+        prob = Prob()
+        model = prob._model
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+
+        model.update()
+        grb_cnt = model.addConstr(grb_var, GRB.EQUAL, 0)
+        model.optimize()
+        var.update()
+
+        e = Expr(f)
+        eq = EqExpr(e, np.array([[4]]))
+        bexpr = BoundExpr(eq, var)
+        prob.add_cnt_expr(bexpr)
+
+        prob.convexify()
+        self.assertTrue(len(prob._penalty_exprs) == 1)
+        self.assertTrue(isinstance(prob._penalty_exprs[0].expr, AbsExpr))
+
+    def test_convexify_leq(self):
+        prob = Prob()
+        model = prob._model
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+
+        model.update()
+        grb_cnt = model.addConstr(grb_var, GRB.EQUAL, 0)
+        model.optimize()
+        var.update()
+
+        e = Expr(f)
+        eq = LEqExpr(e, np.array([[4]]))
+        bexpr = BoundExpr(eq, var)
+        prob.add_cnt_expr(bexpr)
+
+        prob.convexify()
+        self.assertTrue(len(prob._penalty_exprs) == 1)
+        self.assertTrue(isinstance(prob._penalty_exprs[0].expr, HingeExpr))
+
+    def test_get_value(self):
+        """
+        min x^2 st. x == 4
+        when convexified,
+        min x^2 + penalty_coeff*|x-4|
+        when penalty_coeff == 1, solution is x = 0.5 and the value is 3.75
+        (according to Wolfram Alpha)
+
+        when penalty_coeff == 2, solution is x = 1.0 and the value is 7.0
+        (according to Wolfram Alpha)
+        """
+        quad = QuadExpr(np.eye(1), np.zeros((1,1)), np.zeros((1,1)))
+        e = Expr(f)
+        eq = EqExpr(e, np.array([[4]]))
+
+        prob = Prob()
+        model = prob._model
+
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+        model.update()
+
+        obj = BoundExpr(quad, var)
+        prob.add_obj_expr(obj)
+        bexpr = BoundExpr(eq, var)
+        prob.add_cnt_expr(bexpr)
+
+        prob.optimize() # needed to set an initial value
+        prob.convexify()
+        prob.optimize(penalty_coeff=1.0)
+        self.assertTrue(np.allclose(var.get_value(), np.array([[0.5]])))
+        self.assertTrue(np.allclose(prob.get_value(1.0), np.array([[3.75]])))
+
+        prob.optimize(penalty_coeff=2.0)
+        self.assertTrue(np.allclose(var.get_value(), np.array([[1.0]])))
+        self.assertTrue(np.allclose(prob.get_value(2.0), np.array([[7]])))
+
+    def test_get_approx_value(self):
+        """
+        min x^2 st. x == 4
+        when convexified,
+        min x^2 + penalty_coeff*|x-4|
+        when penalty_coeff == 1, solution is x = 0.5 and the value is 3.75
+        (according to Wolfram Alpha)
+
+        when penalty_coeff == 2, solution is x = 1.0 and the value is 7.0
+        (according to Wolfram Alpha)
+        """
+        quad = QuadExpr(np.eye(1), np.zeros((1,1)), np.zeros((1,1)))
+        e = Expr(f)
+        eq = EqExpr(e, np.array([[4]]))
+
+        prob = Prob()
+        model = prob._model
+
+        grb_var = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x')
+        grb_vars = np.array([[grb_var]])
+        var = Variable(grb_vars)
+        model.update()
+
+        obj = BoundExpr(quad, var)
+        prob.add_obj_expr(obj)
+        bexpr = BoundExpr(eq, var)
+        prob.add_cnt_expr(bexpr)
+
+        prob.optimize() # needed to set an initial value
+        prob.convexify()
+        prob.optimize(penalty_coeff=1.0)
+        self.assertTrue(np.allclose(var.get_value(), np.array([[0.5]])))
+        self.assertTrue(np.allclose(prob.get_approx_value(1.0), np.array([[3.75]])))
+
+        prob.optimize(penalty_coeff=2.0)
+        self.assertTrue(np.allclose(var.get_value(), np.array([[1.0]])))
+        self.assertTrue(np.allclose(prob.get_approx_value(2.0), np.array([[7]])))
+
+    def test_pos_grb_var_manager(self):
+        prob = Prob()
+        model = prob._model
+        init_num = 1
+        inc_num = 10
+        shape = (2,7)
+        pos_grb_manager = PosGRBVarManager(model, init_num=init_num, inc_num=inc_num)
+        pgm = pos_grb_manager
+        PosGRBVarManager.INC_NUM = 2
+        self.assertTrue(len(pgm._grb_vars) == init_num)
+        var = pgm.next()
+        test_grb_var_pos(self, var)
+        self.assertTrue(len(pgm._grb_vars) == inc_num+init_num)
+        a = pgm.get_array(shape)
+        for x in a.flatten():
+            test_grb_var_pos(self, x)
+
+        self.assertTrue(a.shape == shape)
+        self.assertTrue(len(pgm._grb_vars) == init_num+2*inc_num)
 
 if __name__ == '__main__':
     unittest.main()
