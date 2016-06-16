0dee2165a9fadf1cc9a0f147161c3d1647d87777
c-l <c.l@berkeley.edu>
2016-06-16 10:10:12 -0700

    implemented prob5-8 from cs287 ps3 q2. solver passes all tests

--- src/test/test_solver.py
+++ src/test/test_solver.py
@@ -34,6 +34,9 @@ def test_prob(ut, x0, x_true, f=zerofunc, g=neginffunc, h=zerofunc,
     Q=np.zeros((N,N)), q=np.zeros((1,N)), A_ineq=np.zeros((1,N)),
     b_ineq=np.zeros((1,1)), A_eq=np.zeros((1,1)), b_eq=np.zeros((1,1))):
 
+    if not np.allclose(A_eq, np.zeros((1,1)))\
+        or not np.allclose(b_eq, np.zeros((1,1))):
+        raise NotImplementedError
     prob = Prob()
     model = prob._model
 
@@ -41,6 +44,7 @@ def test_prob(ut, x0, x_true, f=zerofunc, g=neginffunc, h=zerofunc,
     grb_var2 = model.addVar(lb=-1 * GRB.INFINITY, ub=GRB.INFINITY, name='x2')
     grb_vars = np.array([[grb_var1], [grb_var2]])
     var = Variable(grb_vars, value=x0)
+    model.update()
 
     quad_obj = BoundExpr(QuadExpr(Q, q, np.zeros((1,1))), var)
     prob.add_obj_expr(quad_obj)
@@ -48,9 +52,14 @@ def test_prob(ut, x0, x_true, f=zerofunc, g=neginffunc, h=zerofunc,
     prob.add_obj_expr(nonquad_obj)
 
     cnts = []
+    lin_ineq = LEqExpr(AffExpr(A_ineq, -b_ineq), np.zeros(b_ineq.shape))
+    lin_ineq = BoundExpr(lin_ineq, var)
+    cnts.append(lin_ineq)
+
     nonlin_ineq = LEqExpr(Expr(g), np.zeros(g(np.zeros((2,1))).shape))
     nonlin_ineq = BoundExpr(nonlin_ineq, var)
     cnts.append(nonlin_ineq)
+
     nonlin_eq = EqExpr(Expr(h), np.zeros(g(np.zeros((2,1))).shape))
     nonlin_eq = BoundExpr(nonlin_eq, var)
     cnts.append(nonlin_eq)
@@ -60,7 +69,6 @@ def test_prob(ut, x0, x_true, f=zerofunc, g=neginffunc, h=zerofunc,
 
     solv.solve(prob, method='penalty_sqp')
     x_sol = var.get_value()
-    if not np.allclose(x_sol, x_true, atol=1e-4): st()
     ut.assertTrue(np.allclose(x_sol, x_true, atol=1e-4))
 
 
@@ -83,9 +91,9 @@ class TestSolver(unittest.TestCase):
     def test_prob2(self):
         x0 = np.array([[10.0],[1.0]])
         f = lambda x: np.array([[x[1,0] + 1e-5 + (x[1,0]-x[0,0])**2]])
-        h = lambda x: np.array([[-x[1,0]]])
+        g = lambda x: np.array([[-x[1,0]]])
         x_true = np.array([[0.0],[0.0]])
-        test_prob(self, x0, x_true, f=f, h=h)
+        test_prob(self, x0, x_true, f=f, g=g)
 
     def test_prob3(self):
         x0 = np.array([[10.0],[1.0]])
@@ -101,6 +109,47 @@ class TestSolver(unittest.TestCase):
         x_true = np.array([[0.0],[np.sqrt(3)]])
         test_prob(self, x0, x_true, f=f, h=h)
 
+    def test_prob5(self):
+        x0 = np.array([[0.0],[0.0]])
+        angles = np.transpose(np.array(range(1,7)) * 2*np.pi/6)
+        angles = angles.reshape((6,1))
+        A_ineq = np.hstack((np.cos(angles),np.sin(angles)))
+        b_ineq = np.ones(angles.shape)
+        q = -np.array([[np.cos(np.pi/6), np.sin(np.pi/6)]])
+        x_true = np.array([[1], [np.tan(np.pi/6)]])
+        test_prob(self, x0, x_true, q=q, A_ineq=A_ineq, b_ineq=b_ineq)
+
+    def test_prob6(self):
+        x0 = np.array([[0.0],[0.0]])
+        angles = np.transpose(np.array(range(1,7)) * 2*np.pi/6)
+        angles = angles.reshape((6,1))
+        Q = .1*np.identity(2)
+        A_ineq = np.hstack((np.cos(angles),np.sin(angles)))
+        b_ineq = np.ones(angles.shape)
+        q = -np.array([[np.cos(np.pi/6), np.sin(np.pi/6)]])
+        g = lambda x: .01*(A_ineq.dot(x)-b_ineq)
+        x_true = np.transpose(np.matrix([1, np.tan(np.pi/6)]))
+        test_prob(self, x0, x_true, Q=Q, q=q, g=g)
+
+    def test_prob7(self):
+        x0 = np.array([[0.],[0.]])
+        f = lambda x: np.array([[x[0,0]**4 + x[1,0]**4]])
+        g = lambda x: np.array([[3 - x[0,0] - x[1,0]]])
+        h = lambda x: np.array([[x[0,0] - 2*x[1,0]]])
+        x_true = np.array([[2.],[1.]])
+        test_prob(self, x0, x_true, f=f, g=g, h=h)
+
+    def test_prob8(self):
+        x0 = np.array([[5.0],[5.0]])
+        g = lambda x: np.vstack((\
+            x[0,0]**2 + x[1,0]**2 - 4, \
+            -((x[0,0]-1)**2 +(x[1,0]-1)**2 - 0.25), \
+            -((x[0,0]+1)**2 +(x[1,0]-1)**2 - 0.25), \
+            -((x[0,0])**2 + 7*(x[1,0]+1-x[0,0]**2/2)**2 - 0.8)))
+
+        Q = np.identity(2)
+        x_true = np.array([[0.],[0.]])
+        test_prob(self, x0, x_true, g=g, Q=Q)
 
 if __name__ == '__main__':
     unittest.main()
